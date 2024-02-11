c361c4c775410510ea5e35c70ec46a3b5d8b83bb
Pranita Shedage <pranitashedage1@gmail.com>
2024-02-10 21:51:21 -0500

    sliding window

--- Neetcode/sliging_window/Longest Repeating Character Replacement.py
+++ Neetcode/sliging_window/Longest Repeating Character Replacement.py
@@ -17,5 +17,20 @@ Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
 The substring "BBBB" has the longest repeating letters, which is 4.
 There may exists other ways to achieve this answer too.
 '''
-
-# Testing
+class Solution(object):
+    def characterReplacement(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        count = {}
+        res = 0
+        l = 0
+        for r in range(len(s)):
+            count[s[r]] = 1 + count.get(s[r], 0)
+            while(r-l + 1) - max(count.values()) > k:
+                count[s[l]] -= 1
+                l += 1
+            res = max(res, r - l + 1)
+        return res
