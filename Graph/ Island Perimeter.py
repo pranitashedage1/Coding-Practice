c1befb17698fa99cb839529e8ac3405b4f9c8f0b
Pranita Shedage <pranitashedage1@gmail.com>
2024-06-11 17:09:09 -0400

    island

---
+++ Graph/ Island Perimeter.py
@@ -0,0 +1,37 @@
+'''
+You are given row x col grid representing a map where grid[i][j] = 1 represents land and grid[i][j] = 0 represents water.
+Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
+The island doesn't have "lakes", meaning the water inside isn't connected to the water around the island. One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
+
+Example 1:
+Input: grid = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
+Output: 16
+Explanation: The perimeter is the 16 yellow stripes in the image above.
+
+Example 2:
+Input: grid = [[1]]
+Output: 4
+
+Example 3:
+Input: grid = [[1,0]]
+Output: 4
+'''
+class Solution:
+
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        if grid is None or len(grid) == 0:
+            return 0
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        rows = len(grid)
+        cols = len(grid[0])
+        perimeter = 0
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 1:
+                    for dir1 in directions:
+                        x = i + dir1[0]
+                        y = j + dir1[1]
+                        if (x < 0 or x >= rows or y < 0 or y >= cols or grid[x][y] == 0):
+                            perimeter += 1          
+        return perimeter
+
