af8cb9fb59d4d32da2131af963444bb95f363d0f
Pranita Shedage <pranitashedage1@gmail.com>
2026-07-10 11:38:31 -0400

    updated stack basics

--- Stack/Basics/usingDeque.py
+++ Stack/Basics/usingDeque.py
@@ -1,7 +1,10 @@
-# stack can be implemented by using deque from collections module
-#  deque is preferred as compared to list as for append and pop it gives O(1) complexity as 
-# compared to the list which gives O(n) complexity
-# deque is preferred in the list when we need  quicker append and pop operations from both ends of the container
+# i) Stack can be implemented by using deque from collections module
+
+# ii) deque and list both provide O(1) append() and pop() operations at the right end. 
+# deque is preferred only when insertions and deletions are required from both ends because appendleft() and popleft() are also O(1), whereas list.insert(0) and list.pop(0) are O(n).
+
+# iii) deque is preferred in the list when we need  quicker append and pop operations from both ends of the container
+
 
 from collections import deque
 
--- Stack/Basics/usingLifoQueue.py
+++ Stack/Basics/usingLifoQueue.py
@@ -1,25 +1,45 @@
-# Stack can be implemented using LifoQueue
-from queue import LifoQueue
+# Stack implementation using LifoQueue
 
+from queue import LifoQueue, Empty
+
+# Create a stack with a maximum capacity of 3 elements
 stack = LifoQueue(maxsize=3)
 
-print("stack size before adding elements= ", stack.qsize())
-#  put will act as a push to add content in the stack
+print("Stack size before adding elements =", stack.qsize())
+
+# put() acts as the push operation
 stack.put('a')
 stack.put('b')
 stack.put('c')
-print("stack size after adding elements = ", stack.qsize())
-#  if I execute stack.put() on full queue, it will wait untill free slot is available.
-# stack.put('e')
-# stack.put_nowait() # if I execute this on fill queue, this will raise an exception that queue is full
-# stack.put_nowait('d')
-#  get will act as pop function to get a content in LIFO order
-print("if stack full or not = ", stack.full())   # this method will be removed eventually
-print(stack.get())
-print(stack.get())
-print(stack.get())
-#  print stack after removing all values
-print("stack size after removing all values", stack.qsize())
-print("If stack is empty or not = ", stack.empty()) # this method will be removed eventually
-print(stack.get_nowait()) 
 
+print("Stack size after adding elements =", stack.qsize())
+
+# put() blocks if the stack is full until space becomes available.
+# Uncommenting the next line will block indefinitely because the stack is full.
+# stack.put('d')
+
+# put_nowait() is a non-blocking version of put().
+# It raises queue.Full if the stack is already full.
+# stack.put_nowait('d')
+
+# full() returns True if the stack has reached its maximum size.
+# Note: In multithreaded programs, full() and empty() are only approximate.
+print("Is stack full?", stack.full())
+
+# get() acts as the pop operation and removes elements in LIFO order.
+print(stack.get())    # c
+print(stack.get())    # b
+print(stack.get())    # a
+
+print("Stack size after removing all elements =", stack.qsize())
+
+# empty() returns True if the stack is empty.
+# Note: In multithreaded programs, this result is only approximate.
+print("Is stack empty?", stack.empty())
+
+# get_nowait() is the non-blocking version of get().
+# It raises queue.Empty if the stack is empty.
+try:
+    print(stack.get_nowait())
+except Empty:
+    print("Cannot pop: Stack is empty.")
